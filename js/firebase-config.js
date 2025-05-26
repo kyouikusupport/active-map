363c573574ecf70386f93cc1e25c3ed06f56a617
kyouikusupport <185699892+kyouikusupport@users.noreply.github.com>
2025-05-26 21:43:07 +0900

    Create firebase-config.js

---
+++ js/firebase-config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDzUeXjETsOc21-GQX6g0aw1YOGwdGXfQ4",
+  authDomain: "school-map-b1230.firebaseapp.com",
+  databaseURL: "https://school-map-b1230-default-rtdb.firebaseio.com",
+  projectId: "school-map-b1230",
+  storageBucket: "school-map-b1230.firebasestorage.app",
+  messagingSenderId: "482290643476",
+  appId: "1:482290643476:web:18527e44a23278c894ad01"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
